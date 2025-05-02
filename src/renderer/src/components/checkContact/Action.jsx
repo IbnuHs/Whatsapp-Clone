76084d867aee73fe80029bf170ef897f8ec7d668
IbnuHs <ibnuhs30@gmail.com>
2025-05-02 23:49:35 +0900

    little bit commit

--- src/renderer/src/components/checkContact/Action.jsx
+++ src/renderer/src/components/checkContact/Action.jsx
@@ -1,16 +1,49 @@
 import { Card } from '@material-tailwind/react'
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { Button } from '@material-tailwind/react'
 
 export const Action = () => {
+  const fileinpuRef = useRef(null)
+  let [files, setFiles] = useState(null)
+  let data = ''
+  function clickButton() {
+    fileinpuRef.current.click()
+  }
+  function onUpload(e) {
+    setFiles(e.target.files[0])
+    console.log(e.target.files[0])
+  }
   return (
-    <Card className="w-[80%] mx-auto rounded-md flex flex-row justify-around p-10 bg-gray-300">
-      <button type="button" className="border-2 border-gray-600 rounded-md px-2 bg-gray-200">
-        Show Data
-      </button>
-      <button type="button" className="border-2 border-gray-600 rounded-md px-2 bg-gray-200">
-        Show Data
-      </button>
-      {/* <button>Download</button> */}
-    </Card>
+    <div className="flex flex-col bg-transparent bg-gray-500 border-2 shadow-sm border-gray-400 rounded-md p-4">
+      <div className="border-2 bg-white flex items-center gap-2 mx-auto border-gray-600  w-full py-1 justify-between rounded-lg px-2">
+        <input
+          type="file"
+          className="hidden"
+          accept=".xlsx"
+          onChange={onUpload}
+          ref={fileinpuRef}
+        />
+        <span className="inline-block w-full text-sm text-gray-700">
+          {files ? files.name : 'Upload Your files here...'}
+        </span>
+        <Button size="sm" color="gray" className="bg-black hover:bg-gray-800" onClick={clickButton}>
+          Add
+        </Button>
+      </div>
+      <div className={`${data ? 'flex' : 'hidden'} justify-center gap-4 p-4`}>
+        <button
+          type="button"
+          className="border-2 border-gray-800 rounded-md text-white px-2 bg-gray-400 transition-all hover:bg-gray-600 hover:border-gray-800"
+        >
+          Download
+        </button>
+        <button
+          type="button"
+          className="border-2  rounded-md text-white px-2 border-gray-800 bg-gray-400 transition-all hover:bg-gray-600 hover:border-gray-800"
+        >
+          Show Data
+        </button>
+      </div>
+    </div>
   )
 }
--- src/renderer/src/components/checkContact/CheckContact.jsx
+++ src/renderer/src/components/checkContact/CheckContact.jsx
@@ -1,34 +1,14 @@
 import React, { useRef } from 'react'
 import { Footer } from '../main/footer'
-import { Input, Button } from '@material-tailwind/react'
-import { IoCloudUpload } from 'react-icons/io5'
 import { Table } from './Table'
 import { Action } from './ACtion'
 
 export const CheckContact = () => {
-  const fileinpuRef = useRef(null)
-  function clickButton() {
-    fileinpuRef.current.click()
-  }
   return (
     <section className="w-full max-h-screen overflow-hidden flex flex-col justify-between bg-[#ece5dd] px-10 pt-10 box-border">
       <div className="mx-auto h-[90%] box-border w-full flex flex-col gap-2">
-        <div className="border-2 flex items-center gap-2 max-w-[90%] mx-auto border-gray-600 bg-gray-300 w-full py-1 justify-between rounded-lg px-2">
-          <input type="file" className="hidden" ref={fileinpuRef} />
-          <span className="inline-block w-full text-sm text-gray-700">
-            Upload Your files here...
-          </span>
-          <Button
-            size="sm"
-            color="gray"
-            className="bg-black hover:bg-gray-800"
-            onClick={clickButton}
-          >
-            Add
-          </Button>
-        </div>
         <Action />
-        {/* <Table /> */}
+        <Table />
       </div>
       <Footer />
     </section>
